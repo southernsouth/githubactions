c8a301fd950ed3cadb94e740e38a970306365e53
Красніков Данило <south@Macbooks-MacBook-Pro.local>
2023-03-26 20:37:20 +0300

    Notifications

---
+++ telegram.py
@@ -0,0 +1,14 @@
+import telebot
+from telebot import types
+import os
+
+
+def main():
+    bot = telebot.TeleBot(os.environ['BOT_TOKEN'])
+    chat_id = os.environ['CHAT_ID']
+    text = "New Pull Request created! Check it out: " + os.environ['GITHUB_SERVER_URL'] + '/' + os.environ['GITHUB_REPOSITORY'] + '/pull/' + os.environ['GITHUB_REF'].split('/')[2]
+    bot.send_message(chat_id, text)
+
+
+if __name__ == '__main__':
+    main()
